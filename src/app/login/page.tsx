d69ada4cf68f42cbc146ec8f0b6998aae3d795aa
Anshul <anshul>
2024-09-06 01:41:02 +0530

    add: test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
         <h1 className="text-5xl font-bold">Login now!</h1>
         <p className="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-          quasi. In deleniti eaque aut repudiandae et a id nisi.
+          quasi. test In deleniti eaque aut repudiandae et a id nisi.
         </p>
       </div>
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
